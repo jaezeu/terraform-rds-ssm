140b62ba31ccf5059a00d91ae43089500ee2f02f
jaezeu <jazeel.meerasah@gmail.com>
2024-12-07 09:21:00 +0800

    add output

--- output.tf
+++ output.tf
@@ -2,3 +2,8 @@ output "rds_endpoint" {
   description = "RDS endpoint name"
   value       = aws_db_instance.default.endpoint
 }
+
+output "instance_id" {
+  description = "ID of your EC2 instance"
+  value = aws_instance.private_ec2.id
+}
